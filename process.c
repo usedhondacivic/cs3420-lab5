56f7a8255d7cbd78cad3af6bd128ca2b573419e0
usedhondacivic <micacer48@gmail.com>
2022-04-26 14:01:58 -0400

    stuff

--- process.c
+++ process.c
@@ -1,11 +1,17 @@
 #include "3140_concur.h"
 #include "realtime.h"
+#include "3140_concur.h"
 #include <stdlib.h>
 #include <MKL46Z4.h>
 #include <assert.h>
 #include <stdbool.h>
 
 process_t *current_process = NULL;
+
+volatile extern realtime_t current_time;
+
+int process_deadline_met;
+int process_deadline_miss;
 
 bool first_select = true;
 
@@ -17,18 +23,21 @@ struct process_state {
 	int n; // Size allocated
 };
 
-typedef struct double_linked_list {
-	struct node *list_start = NULL;
-	struct node *list_end = NULL;
-} double_linked_list;
-
 typedef struct node {
-	process_t val;
+	process_t *val;
 	struct node *prev;
 	struct node *next;
 } node;
 
-struct double_linked_list queue;
+typedef struct double_linked_list {
+	struct node *list_start;
+	struct node *list_end;
+} double_linked_list;
+
+struct double_linked_list scheduler = {
+	.list_start = NULL,
+	.list_end = NULL
+};
 
 // adds element to the beginning of the list
 void add_elem_begin(double_linked_list * list, node * elem) {
@@ -51,7 +60,7 @@ void add_elem_end(double_linked_list * list, node * elem) {
 	} else {
 		assert(list->list_end->next == NULL);
 		list->list_end->next = elem;
-		elem->prev = list_end;
+		elem->prev = list->list_end;
 
 		list->list_end = elem;
 		elem->next = NULL;
@@ -83,20 +92,27 @@ node * remove_first_elem(double_linked_list * list) {
 int process_create (void(*f) (void), int n){
 	// Make an element for the queue containing info about the process
 	node *new_elem_ptr = malloc(sizeof(node));
+	new_elem_ptr->val = malloc(sizeof(process_t));
+
 	if(new_elem_ptr == NULL){
 		return -1;
 	}
-	new_elem_ptr->val.sp = process_stack_init(f, n);
-	new_elem_ptr->val.orig_sp = new_elem_ptr->val.sp;
-	new_elem_ptr->val.n = n;
+
+	new_elem_ptr->val->sp = process_stack_init(f, n);
+	new_elem_ptr->val->orig_sp = new_elem_ptr->val->sp;
+	new_elem_ptr->val->n = n;
 	new_elem_ptr->prev = NULL;
 	new_elem_ptr->next = NULL;
 
-	if(new_elem_ptr->val.sp == NULL) return -1; // If the new stack cannot be allocated, return and error
+	if(new_elem_ptr->val->sp == NULL) return -1; // If the new stack cannot be allocated, return and error
 
-	add_elem_end(new_elem_ptr); // Add the new element to the queue
+	add_elem_end(&scheduler, new_elem_ptr); // Add the new element to the queue
 
 	return 0;
+}
+
+int process_rt_create(void (*f)(void), int n, realtime_t* start, realtime_t* deadline){
+	return -1;
 }
 
 void process_start (void){
@@ -108,32 +124,40 @@ void process_start (void){
 	PIT->CHANNEL[0].LDVAL = 0x00004E20; // 20k cycles @ 10Mhz = 2 ms before switching processes
 	PIT->CHANNEL[0].TCTRL |= (1 << 1); // Enable interrupts for channel 0
 
+	PIT->CHANNEL[1].LDVAL = 0x000028F6; // 10,486 cycles @ 10.4Mhz = 1 ms per interupt
+	PIT->CHANNEL[1].TCTRL |= (1 << 1); // Enable interrupts for channel 1
+
 	PIT->CHANNEL[0].TCTRL |= 0x1; //Enable channel 0
+	PIT->CHANNEL[1].TCTRL |= 0x1; //Enable channel 1
 
 	process_begin(); // Initialize the first process
 }
 
 unsigned int * process_select(unsigned int * cursp){
-	double_linked_list *fst = remove_first_elem(); //Check the first element
+	node *fst = remove_first_elem(&scheduler); //Check the first element
 	if(cursp == NULL){ // Either the running process finished or it is the first time process_select is being called
 		if(!first_select){ //If its not the first time, free memory from the running process
-			process_stack_free(fst->val.orig_sp, fst->val.n);
+			process_stack_free(fst->val->orig_sp, fst->val->n);
 			free(fst);
 			current_process = NULL;
 		}else{ // If it is the first time, put the element back in the front of the queue
-			add_elem_begin(fst);
+			add_elem_begin(&scheduler, fst);
 			first_select = false;
 		}
 	}else{
 		//Put running process back into queue
-		fst->val.sp = cursp;
-		add_elem_end(fst);
+		fst->val->sp = cursp;
+		add_elem_end(&scheduler, fst);
 	}
 
 	//Get next process from linked list, return stack pointer
-	if(list_start == NULL){
+	if(scheduler.list_start == NULL){
 		return NULL;
 	}
-	current_process = &(list_start->val);
+	current_process = scheduler.list_start->val;
 	return current_process->sp;
+}
+
+void PIT1_Service(){
+	while(1);
 }
